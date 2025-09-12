8cc53201e92394dc1742320f484b46ccb923c5fc
Aziel202 <75705656+Aziel202@users.noreply.github.com>
2025-09-12 14:29:57 -0400

    test taking pictures

---
+++ Vision/modules/Chess/demos/take_picture.py
@@ -0,0 +1,31 @@
+import depthai as dai
+import cv2
+
+# Create pipeline
+pipeline = dai.Pipeline()
+
+# Define source and output
+camRgb = pipeline.create(dai.node.ColorCamera)
+xoutRgb = pipeline.create(dai.node.XLinkOut)
+
+# Set properties
+camRgb.setPreviewSize(640, 480)
+camRgb.setInterleaved(False)
+camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
+
+# Linking
+camRgb.preview.link(xoutRgb.input)
+xoutRgb.setStreamName("rgb")
+
+# Connect to device and start pipeline
+with dai.Device(pipeline) as device:
+    # Get output queue
+    qRgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
+    
+    # Take a picture
+    inRgb = qRgb.get()  # blocking call, will wait until a new data has arrived
+    frame = inRgb.getCvFrame()
+    
+    # Save the image
+    cv2.imwrite("oakd_picture.jpg", frame)
+    print("Picture saved as oakd_picture.jpg")
